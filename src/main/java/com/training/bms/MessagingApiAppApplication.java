d84bbdb325dcc9e54e77be1a3732d898cae81ff5
ArjunDevSingla <arjundevsingla1612@gmail.com>
2024-07-12 15:17:33 +0530

    Created endpoint

--- src/main/java/com/training/bms/MessagingApiAppApplication.java
+++ src/main/java/com/training/bms/MessagingApiAppApplication.java
@@ -1,10 +1,23 @@
 package com.training.bms;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MessagingApiAppApplication {
+	
+	@Autowired
+	Weekend week;
+	
+	@GetMapping("happyWeekend")
+	public String weekendComing()
+	{
+		return week.weekEndCome();
+	}
 	
 	public static String printGreetings()
 	{
---
+++ src/main/java/com/training/bms/Weekend.java
@@ -0,0 +1,12 @@
+package com.training.bms;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Weekend {
+	
+	public String weekEndCome()
+	{
+		return "Saturday and Sunday";
+	}
+}
